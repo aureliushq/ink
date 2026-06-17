d3016cf093628657dd3c02f59e8983cca27f639e
Ilango <rajagopalilango@gmail.com>
2026-06-17 08:05:07 +0530

    add: template detection and parsing

---
+++ internal/renderer/templates.go
@@ -0,0 +1,76 @@
+package renderer
+
+import (
+	"bytes"
+	"html/template"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/aureliushq/ink/internal/config"
+)
+
+type TemplateCache struct {
+	Files map[string]*template.Template
+}
+
+func NewTemplateCache() *TemplateCache {
+	return &TemplateCache{
+		Files: map[string]*template.Template{},
+	}
+}
+
+type TemplateData struct {
+	SiteTitle    string
+	SiteSubtitle string
+	Title        string
+	Description  string
+	Subtitle     string
+	PageURL      string
+	Content      template.HTML
+}
+
+func NewTemplateData() TemplateData {
+	return TemplateData{}
+}
+
+func (tc *TemplateCache) Setup(cfg *config.Config) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	fileSystem := os.DirFS(cwd)
+	templatePath := path.Join("themes", cfg.Theme.Name, "*.html")
+	pages, err := fs.Glob(fileSystem, templatePath)
+	if err != nil {
+		return err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		patterns := []string{
+			path.Join("themes", cfg.Theme.Name, "base.html"),
+			path.Join("themes", cfg.Theme.Name, "partials", "header.html"),
+			page,
+		}
+
+		ts := template.Must(template.New(name).ParseFS(fileSystem, patterns...))
+
+		tc.Files[name] = ts
+	}
+
+	return nil
+}
+
+func (tc *TemplateCache) Execute(name string, templateData TemplateData) (string, error) {
+	html := new(bytes.Buffer)
+
+	if err := tc.Files[name].ExecuteTemplate(html, "base", templateData); err != nil {
+		return "", err
+	}
+
+	return html.String(), nil
+}
