8714c6595d8c87f35dc89c3ddb5acd25b8ee2900
Ilango <rajagopalilango@gmail.com>
2026-06-17 08:10:07 +0530

    update: ink.toml and gitignore

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2026 Ilango Rajagopal <hey@i4o.dev>
 */
 package main
 
-import "github.com/aureliushq/ink/cmd"
+import (
+	"github.com/aureliushq/ink/cmd"
+)
 
 func main() {
 	cmd.Execute()
