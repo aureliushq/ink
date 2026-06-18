ffb9e08ae8c92c115d3e2d8f63ae41110ab315e0
Ilango <rajagopalilango@gmail.com>
2026-06-19 02:46:04 +0530

    update: build package - move pre-run logic here

--- internal/build/build.go
+++ internal/build/build.go
@@ -1,5 +1,50 @@
 package build
 
 import (
-	"fmt"
+	"embed"
+
+	"github.com/aureliushq/ink/internal/config"
+	"github.com/aureliushq/ink/internal/content"
+	"github.com/aureliushq/ink/internal/renderer"
+	"github.com/charmbracelet/log"
 )
+
+func InitConfig() (*config.Config, error) {
+	cfg := config.NewConfig()
+	if err := config.Setup(cfg); err != nil {
+		return &config.Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func InitTemplateCache(cfg *config.Config, logger *log.Logger, themesFS embed.FS) (*renderer.TemplateCache, error) {
+	templateCache := renderer.NewTemplateCache()
+	if err := templateCache.Setup(cfg, themesFS); err != nil {
+		return &renderer.TemplateCache{}, err
+	}
+	if err := templateCache.Overrides(cfg, logger); err != nil {
+		return &renderer.TemplateCache{}, err
+	}
+	return templateCache, nil
+}
+
+func ReadContent(buildConfig config.BuildConfig, logger *log.Logger) ([]content.Content, error) {
+	paths, err := content.DiscoverFiles(buildConfig.ContentDir, logger)
+	if err != nil {
+		return []content.Content{}, err
+	}
+	allContent := []content.Content{}
+	for _, path := range paths {
+		data := content.NewContent()
+		data.SourcePath = path
+
+		err := data.Unmarshal(buildConfig)
+		if err != nil {
+			return []content.Content{}, err
+		}
+		allContent = append(allContent, data)
+	}
+
+	return allContent, nil
+}
